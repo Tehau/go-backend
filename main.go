11eb25226f9d1b08279fc25085adb216c4666d26
Tehau <tsing.tehau@gmail.com>
2022-12-15 20:13:31 +0100

    get character by id api

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -34,6 +37,23 @@ type Character struct {
 	Created time.Time `json:"created"`
 }
 
+func characterReader() CharacterNodes {
+
+	jsonFile, err := os.Open("data/rickandmortycharacter.json")
+
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	data := CharacterNodes{}
+	err = json.NewDecoder(jsonFile).Decode(&data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer jsonFile.Close()
+	return data
+}
+
 type fooHandler struct {
 	Message string
 }
@@ -49,20 +69,11 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 func charactersHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		jsonFile, err := os.Open("data/rickandmortycharacter.json")
-
-		// if we os.Open returns an error then handle it
-		if err != nil {
-			fmt.Println(err)
-		}
-		data := CharacterNodes{}
-		err = json.NewDecoder(jsonFile).Decode(&data)
-		if err != nil {
-			log.Fatal(err)
-		}
-		_characterJson, err := json.Marshal(data.CharacterNodes)
+		data := characterReader()
+		_characterJson, err := json.MarshalIndent(data.CharacterNodes, "", "    ")
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(_characterJson)
@@ -70,28 +81,68 @@ func charactersHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func characterHandler(w http.ResponseWriter, r *http.Request) {
+	urlPathSegments := strings.Split(r.URL.Path, "character/")
+
+	charID, err := strconv.Atoi(urlPathSegments[len(urlPathSegments)-1])
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	character, itemId := findCharacterByID(charID)
+	if character == nil {
+		fmt.Println("Error at ID : ", itemId)
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	case http.MethodGet:
+		_characterJson, err := json.MarshalIndent(character, "", "    ")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(_characterJson)
+	case http.MethodPut:
+		var updatedCharacter Character
+		bodyBytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatalln(err)
+			return
+		}
+		err = json.Unmarshal(bodyBytes, &updatedCharacter)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if updatedCharacter.Id != charID {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		character = &updatedCharacter
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+}
+
+func findCharacterByID(id int) (*Character, int) {
+	data := characterReader()
+	if len(data.CharacterNodes) >= id {
+		return &data.CharacterNodes[id-1], id
+	}
+	return nil, 0
+}
+
 func main() {
-	jsonFile, err := os.Open("data/rickandmortycharacter.json")
-
-	// if we os.Open returns an error then handle it
-	if err != nil {
-		fmt.Println(err)
-	}
-	data := CharacterNodes{}
-	err = json.NewDecoder(jsonFile).Decode(&data)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//for i := 0; i < len(data.CharacterNodes); i++ {
-	//	fmt.Printf("Name: %s\n", data.CharacterNodes[i].Name)
-	//	fmt.Printf("Status: %s\n", data.CharacterNodes[i].Status)
-	//}
 
 	http.Handle("/foo", &fooHandler{Message: "hello World"})
 	http.HandleFunc("/bar", barHandler)
 	http.HandleFunc("/character", charactersHandler)
+	http.HandleFunc("/character/", characterHandler)
+
 	http.ListenAndServe(":5000", nil)
 	// defer the closing of our jsonFile so that we can parse it later on
-	defer jsonFile.Close()
 }
