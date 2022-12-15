ffa6e239afbcedde787d255a9a12c8bae588cd65
Tehau <tsing.tehau@gmail.com>
2022-12-15 19:16:00 +0100

    add new api /character

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -45,6 +46,30 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("bar called"))
 }
 
+func charactersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		jsonFile, err := os.Open("data/rickandmortycharacter.json")
+
+		// if we os.Open returns an error then handle it
+		if err != nil {
+			fmt.Println(err)
+		}
+		data := CharacterNodes{}
+		err = json.NewDecoder(jsonFile).Decode(&data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_characterJson, err := json.Marshal(data.CharacterNodes)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(_characterJson)
+
+	}
+}
+
 func main() {
 	jsonFile, err := os.Open("data/rickandmortycharacter.json")
 
@@ -55,14 +80,17 @@ func main() {
 	data := CharacterNodes{}
 	err = json.NewDecoder(jsonFile).Decode(&data)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	for i := 0; i < len(data.CharacterNodes); i++ {
-		fmt.Println("Name: ", data.CharacterNodes[i].Name)
-		fmt.Println("Status: ", data.CharacterNodes[i].Status)
-	}
+
+	//for i := 0; i < len(data.CharacterNodes); i++ {
+	//	fmt.Printf("Name: %s\n", data.CharacterNodes[i].Name)
+	//	fmt.Printf("Status: %s\n", data.CharacterNodes[i].Status)
+	//}
+
 	http.Handle("/foo", &fooHandler{Message: "hello World"})
 	http.HandleFunc("/bar", barHandler)
+	http.HandleFunc("/character", charactersHandler)
 	http.ListenAndServe(":5000", nil)
 	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
